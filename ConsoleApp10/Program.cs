8f0cf33a2f1f219b6568cee18ccda80e64755a3c
Beyza <112386181+Kirtan51@users.noreply.github.com>
2022-10-18 13:02:51 +0300

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //kullanıcıdan rastegele sayı al,tahmin al,bilemezse tahmin sayacını arttır,bilirse tebrikler
+            Random sayiGen = new Random();
+            int sayi = sayiGen.Next(1, 101);
+            bool bulundu = false;
+            int tahminSayisi = 1;
+            while (bulundu==false)
+            {
+                Console.WriteLine("1-100 arası");
+                int tahmin = Convert.ToInt32(Console.ReadLine());
+                if(tahmin==sayi)
+                {
+
+                    bulundu = true;
+                    Console.WriteLine("tebrikler{0}", tahminSayisi);
+
+
+                }
+                else
+                {
+                    tahminSayisi++;
+                    if (tahmin > sayi)
+                    {
+                        Console.WriteLine("tahmini kucult");
+                    }
+                    else
+                    { 
+                        Console.WriteLine("tahmini büyüt");
+                    }
+                        
+                        
+                        
+                }
+
+
+            }
+        
+        
+        
+        }
+    }
+}
